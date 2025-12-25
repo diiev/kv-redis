093752223f1ec12d522f36b340ffc2fc630eb499
diiev <ramzans096@gmail.com>
2025-12-25 16:25:16 +0300

    add confif file and func parser

--- main.go
+++ main.go
@@ -1,66 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
-	"strconv"
 )
-
-type ValueType string
-
-const (
-	ARRAY  ValueType = "*"
-	BULK   ValueType = "$"
-	STRING ValueType = "+"
-	ERROR  ValueType = "-"
-	NULL   ValueType = ""
-)
-
-type Value struct {
-	typ   ValueType
-	bulk  string
-	str   string
-	err   string
-	array []Value
-}
-
-func (v *Value) readArray(reader io.Reader) {
-	buf := make([]byte, 4)
-	reader.Read(buf)
-	fmt.Println(string(buf))
-	arrLen, err := strconv.Atoi(string(buf[1]))
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for range arrLen {
-		bulk := v.readBulk(reader)
-		v.array = append(v.array, bulk)
-	}
-}
-
-func (v *Value) readBulk(reader io.Reader) Value {
-	buf := make([]byte, 4)
-	reader.Read(buf)
-
-	n, err := strconv.Atoi(string(buf[1]))
-	if err != nil {
-		fmt.Println(err)
-		return Value{}
-	}
-	bulkBuf := make([]byte, n+2)
-	reader.Read(bulkBuf)
-	bulk := string(bulkBuf[:n])
-
-	return Value{typ: BULK, bulk: bulk}
-}
 
 func main() {
+	log.Println("Reading config file")
+	readConf("./redis.conf")
 	l, err := net.Listen("tcp", ":6379")
 
 	if err != nil {
@@ -85,85 +34,4 @@ func main() {
 
 	}
 
-}
-
-type Handler func(*Value) *Value
-
-var Handlres = map[string]Handler{
-	"COMMAND": command,
-	"GET":     get,
-	"SET":     set,
-}
-var DB = map[string]string{}
-
-func command(v *Value) *Value {
-	return &Value{typ: STRING, str: "OK"}
-}
-func get(v *Value) *Value {
-	args := v.array[1:]
-	if len(args) != 1 {
-		return &Value{typ: ERROR, err: "ERR invalid number of arguments for 'GET' "}
-	}
-	name := args[0].bulk
-	val, ok := DB[name]
-	if !ok {
-		return &Value{typ: NULL}
-	}
-	return &Value{typ: BULK, bulk: val}
-
-}
-
-func set(v *Value) *Value {
-	args := v.array[1:]
-	if len(args) != 2 {
-		return &Value{typ: ERROR, err: "ERR invalid number of arguments for 'SET' "}
-	}
-	key := args[0].bulk
-	val := args[1].bulk
-
-	DB[key] = val
-
-	return &Value{typ: STRING, str: "OK"}
-}
-
-func handler(conn net.Conn, v *Value) {
-	cmd := v.array[0].bulk
-	handler, ok := Handlres[cmd]
-	if !ok {
-		fmt.Println("Invalid Command", cmd)
-		return
-
-	}
-
-	reply := handler(v)
-	w := NewWriter(conn)
-	w.Write(reply)
-}
-
-type Writer struct {
-	writer io.Writer
-}
-
-func NewWriter(w io.Writer) *Writer {
-	return &Writer{
-		writer: bufio.NewWriter(w),
-	}
-
-}
-
-func (w *Writer) Write(v *Value) {
-	var reply string
-	switch v.typ {
-	case STRING:
-		reply = fmt.Sprintf("%s%s\r\n", v.typ, v.str)
-	case BULK:
-		reply = fmt.Sprintf("%s%d\r\n%s\r\n", v.typ, len(v.bulk), v.bulk)
-	case ERROR:
-		reply = fmt.Sprintf("%s%s", v.typ, v.err)
-	case NULL:
-		reply = "$-1\r\n"
-	}
-
-	w.writer.Write([]byte(reply))
-	w.writer.(*bufio.Writer).Flush()
 }
